3385c6cc7672c99f421b92e079271376ada49cff
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2019-11-12 14:32:57 +0000

    add new todo list page

---
+++ BlazorSPA/Pages/ToDo.razor.cs
@@ -0,0 +1,24 @@
+﻿using BlazorSPA.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSPA.Pages
+{
+    public class ToDoBase : ComponentBase
+    {
+        protected IList<ToDoItem> todos = new List<ToDoItem>();
+        protected string newTodo;
+
+        protected void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new ToDoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+    }
+}
