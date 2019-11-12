1cf662faa5324e47f5527151026eb212fa8cf3b7
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2019-11-12 16:09:10 +0000

    implement new page components implement callback logic from child to parent component

---
+++ BlazorSPA/Shared/Components/ChildComponent.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSPA.Shared.Components
+{
+    public class ChildComponentBase : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+    }
+}
---
+++ BlazorSPA/Shared/Components/ParentComponent.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSPA.Shared.Components
+{
+    public class ParentComponentBase : ComponentBase
+    {
+        protected string messageText;
+
+        protected void ShowMessage(MouseEventArgs e)
+        {
+            messageText = "Blaze a new trail with Blazor!";
+        }
+    }
+}
